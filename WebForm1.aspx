34525153595c7f2a7f46711ca4c246b129e01925
BK3820 <bala74573@gmail.com>
2022-05-09 09:51:58 +0530

    First commit

---
+++ WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASPloginPractice.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            UserName: <asp:TextBox ID="TxtUsername" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Password:
+            <asp:TextBox TextMode="Password" ID="TxtPassword"  runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="messagelbl" runat="server" Enabled="False" Text="Label" Visible="False"></asp:Label>
+        </div>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Login" Width="145px" OnClick="Button1_Click1" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPloginPractice
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        SqlConnection SqlCon = new SqlConnection("Data Source=LAPTOP-OJ1LCGF3; Initial Catalog=ASPLogin; Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlDataAdapter SQLAdapter = new SqlDataAdapter("SELECT * FROM useraccount WHERE UserName='" + TxtUsername.Text + "' AND Password='" + TxtPassword.Text + "'", SqlCon);
+            DataTable DT = new DataTable();
+            SQLAdapter.Fill(DT);
+
+            if (DT.Rows.Count > 0)
+            {
+                messagelbl.Visible = true;
+                messagelbl.Text = "Succesfully loggedin";
+                messagelbl.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                messagelbl.Text = "Invalid UserName or Password";
+                messagelbl.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
